f7ff98123f0bdcfa30aeba0844c3c91368996d83
Waterme1onDog <128970523+Waterme1onDog@users.noreply.github.com>
2023-06-18 14:17:00 +0500

    6 задача

---
+++ task6.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+    if (num1 % 2==0)
+    {
+        Console.Write("Число четное");
+    }
+    else
+    {
+        Console.Write("Число нечетное");
+    }
