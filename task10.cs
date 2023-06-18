c45eae4efc10f15da3d44094dc9114ed4b4cca7c
Waterme1onDog <128970523+Waterme1onDog@users.noreply.github.com>
2023-06-18 14:17:55 +0500

    10 задача

---
+++ task10.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трехзначное число: ");
+int TripleNumUser = Convert.ToInt32(Console.ReadLine());
+int TripleNum = TripleNumUser;
+    TripleNum = TripleNum /10; 
+    TripleNum = TripleNum %10;
+Console.WriteLine($"Вторая цифра числа {TripleNumUser} = {TripleNum}");
