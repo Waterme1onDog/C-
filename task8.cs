f31b32381b426badbe33a2efa98f908243393ffb
Waterme1onDog <128970523+Waterme1onDog@users.noreply.github.com>
2023-06-18 14:17:25 +0500

    8 задача

---
+++ task8.cs
@@ -0,0 +1,12 @@
+﻿int firstnumber = 1;
+Console.WriteLine("Введите число: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Чётные числа от 1 до {numN}");
+while (firstnumber <= numN)
+{
+    if (firstnumber % 2 !=1)
+    {
+        Console.Write(firstnumber + ", ");
+    }
+    firstnumber++;
+}
