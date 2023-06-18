5f1cd4519387aca1281fe7a5088ec80748fd21f8
Waterme1onDog <128970523+Waterme1onDog@users.noreply.github.com>
2023-06-18 14:18:28 +0500

    13 задача

---
+++ task13.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число дня недели от 1 до 7: ");
+int DayWeek = Convert.ToInt32(Console.ReadLine());
+    if (DayWeek >= 6 && DayWeek <=7 )
+    {
+        Console.Write($"{DayWeek} - да, это число является выходным днем ");
+    }
+    else
+    {
+        Console.Write($"{DayWeek} - нет, это число не является выходным днем!!! ");
+    }
