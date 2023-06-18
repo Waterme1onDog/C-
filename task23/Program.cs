d38c6ea95dd0b6c0e0e714d872b251c5f7860812
WatermelonDog <arhy.bes@mail.ru>
2023-06-18 13:47:24 +0500

    Задача23

---
+++ task23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите значение N для вычисления таблицы кубов:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+for (int i=1;i<=numberN;i++)
+{
+int cubenumbers = i*i*i;
+Console.Write($"{cubenumbers} ");
+}
