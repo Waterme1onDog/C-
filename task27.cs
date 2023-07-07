33445b2abec288e129b9540e8020a7a2cec1780c
Waterme1onDog <128970523+Waterme1onDog@users.noreply.github.com>
2023-07-07 12:39:49 +0500

    27 задача

---
+++ task27.cs
@@ -0,0 +1,19 @@
+﻿int number = Prompt("Введите число:");
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int SumAllDigit(int number)
+{
+    int result = 0;
+    while (number>0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+Console.Write($"Сумма цифр в чесле {number} = {SumAllDigit(number)}");
