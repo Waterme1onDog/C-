de043e9f1e8a08742ca39e93c1b69c1bdd585641
Waterme1onDog <128970523+Waterme1onDog@users.noreply.github.com>
2023-07-12 12:14:20 +0500

    38 задача

---
+++ task38.cs
@@ -0,0 +1,29 @@
+﻿double[] array = new double[5];
+Random rand = new Random();
+double maxNumber = array[0];
+double minNumber = array[0];
+double difNumbers = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-100, 100) + rand.NextDouble() ;                               //Создаем вещественный массив
+
+    if (array[i] < minNumber )                                                          //Вычисляем меньшее и большее значение
+    {
+        minNumber = array[i];
+    }
+    if(array[i] > maxNumber)
+    {
+        maxNumber=array[i];
+    }                                                      
+}                                                                      
+difNumbers = maxNumber + minNumber ;                                                                          //Складываем меньшее  и большее 
+var str = string.Join(" ", array);Console.WriteLine(str);                                                     //Выводим массив , округляем ответ
+Console.WriteLine(string.Format("{0:F1}", ($"Минимальное значение = {minNumber}")));
+Console.WriteLine(string.Format("{0:F1}", ($"Максимальное значени = {maxNumber}"))); 
+Console.WriteLine($"Разница между минимальным и максимальным значением = {Math.Round(difNumbers,2)} ");
+
+
+
+
+
+
