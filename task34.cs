84961a7a258dcab6502a29ab6d4990e3f9cbb845
Waterme1onDog <128970523+Waterme1onDog@users.noreply.github.com>
2023-07-12 12:01:17 +0500

    34 задача

---
+++ task34.cs
@@ -0,0 +1,16 @@
+﻿int positiveNumber = 0;
+Random rnd = new Random();
+int a = rnd.Next(100,999);
+int b = rnd.Next(100,999);
+int c = rnd.Next(100,999);
+int d = rnd.Next(100,999);
+int[] array = {a,b,c,d};
+foreach (int i in array)
+{
+    if (i % 2 == 0 )
+    {
+       positiveNumber++;
+    }
+    System.Console.Write("{0} ", i);
+}
+System.Console.Write($"Массив имеет {positiveNumber} чётных чисел ");
