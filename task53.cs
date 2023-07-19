be5b0a8bbe60e87646b4dbef21523335e5a3243e
Waterme1onDog <128970523+Waterme1onDog@users.noreply.github.com>
2023-07-19 10:46:40 +0500

    53 задача

---
+++ task53.cs
@@ -0,0 +1,39 @@
+﻿int lines = 3;
+int columns = 4;
+double[,] numbers = new double[lines, columns];
+fillArrayRandomNumbers(numbers);
+printArray(numbers);
+
+void fillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(0, 10));
+        }
+    }
+}
+
+void printArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        sum += numbers[i, j];
+    }
+    Console.Write($"{ Math.Round (sum / numbers.GetLength(0),2)} ");
+}
