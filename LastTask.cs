1777b2cdf758e465065b991570fb11f433daba4d
Waterme1onDog <128970523+Waterme1onDog@users.noreply.github.com>
2023-08-02 19:41:13 +0500

    LastTask1

---
+++ LastTask.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите значение M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N:");
+int n = Convert.ToInt32(Console.ReadLine());
+void number(int m, int n)
+{
+if (m > n)
+return;
+if (m % 2 == 0)
+{
+Console.Write($"{m} ");
+}
+number(m+1,n);
+}
+number(m,n);
